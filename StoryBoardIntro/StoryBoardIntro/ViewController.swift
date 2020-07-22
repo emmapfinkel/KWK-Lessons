e75d028b51b686366479fc66291649a839e12b88
emmapfinkel <63925401+emmapfinkel@users.noreply.github.com>
2020-07-22 11:48:58 -0400

    Intro to Story Board Iteration 1

---
+++ StoryBoardIntro/StoryBoardIntro/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  StoryBoardIntro
+//
+//  Created by Emma Finkel on 7/22/20.
+//  Copyright © 2020 KWK. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+
+    }
+
+
+}
+
